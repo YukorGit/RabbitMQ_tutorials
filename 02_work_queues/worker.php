4bcf16c7f2d15710ab7ef5ba62d7ac0677c49369
yuriy <yuriyseraz@gmail.com>
2025-05-15 14:33:02 +0300

    Adds work_queues

---
+++ 02_work_queues/worker.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+
+$callback = function ($msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+    $sleepTime = substr_count($msg->body, '.');
+    sleep($sleepTime);
+    echo " [x] Done\n";
+
+    $msg->ack();
+};
+
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+try {
+    $channel->consume();
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
+}
+
+$channel->close();
+$connection->close();
